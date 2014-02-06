028d78d7908d3c61d59b4f14ff7e29e98fddac67
Joe Heyming <joeheyming@gmail.com>
2014-02-06 12:36:51 -0800

    Fixed syntax error.

--- nyan.sh
+++ nyan.sh
@@ -56,10 +56,10 @@ Running NYAN!
 EOF
 BASEDIR=`dirname $0`;
 chmod 777 /tmp/nyan;
-frames=`ls $BASEDIR/frames/ | sort`;
+frames=`ls $BASEDIR/frames/ | grep txt$ | sort`;
 while true; 
 do
-    for x in $frames | grep txt`; do 
+    for x in $frames; do 
         cat $BASEDIR/frames/$x > /tmp/nyan;
         sleep .2;
 	nextFrame;
